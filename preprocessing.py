c319fa1f0685cb3235a2af122865dde69af00f78
Yiwen Xu <yiwen@Yiwens-MacBook-Pro.local>
2018-11-12 22:02:38 -0500

    preprocess dev set; sort LABEL_LIST

--- preprocessing.py
+++ preprocessing.py
@@ -1,15 +1,15 @@
 import numpy as np
 
+########## Preprocessing Training Dataset ##########
 train_data = np.load("./data/train.npy", encoding='bytes')
 train_label = np.load("./data/train_transcripts.npy", encoding='bytes') # 24724 utterances
-
-train_label = train_label
 
 # Convert bytes to string
 for i in range(len(train_label)):
     train_label[i] = train_label[i].astype(str)
 
-# character based label
+# Create vocabulary list and vocabulary map
+# character-based label
 LABEL_LIST = ['0'] # 33 characters
 LABEL_MAP = {'0':0}
 for i in range(len(train_label)):
@@ -20,24 +20,27 @@ for i in range(len(train_label)):
                 LABEL_MAP[char] = len(LABEL_LIST) - 1
 LABEL_LIST.append('<space>')
 LABEL_MAP['<space>'] = len(LABEL_LIST) - 1
+LABEL_LIST = sorted(LABEL_LIST)
+for key in LABEL_MAP.keys():
+    LABEL_MAP[key] = LABEL_LIST.index(key)
 print(LABEL_LIST)
 print(LABEL_MAP)
-# print(train_label)
 
+# Convert transcripts to character-based label
 char_label = []
 for i in range(len(train_label)):
     tmp_label = []
-    tmp_label.append(0)
+    tmp_label.append(LABEL_MAP['0'])
     for j in range(len(train_label[i])):
         for k in range(len(list(train_label[i][j]))):
             key = list(train_label[i][j])[k]
             tmp_label.append(LABEL_MAP[key])
         if j != len(train_label[i]) - 1:
             tmp_label.append(LABEL_MAP['<space>'])
-    tmp_label.append(0)
+    tmp_label.append(LABEL_MAP['0'])
     char_label.append(tmp_label)
-# print(char_label)
 
+# Save characters-based label to file
 np.save('./data/train_char.npy', np.array(char_label))
 
 assert len(LABEL_LIST) == len(LABEL_MAP)
@@ -45,3 +48,30 @@ assert len(set(LABEL_LIST)) == len(LABEL_MAP)
 
 data = np.load('./data/train_char.npy')
 assert len(data) == len(train_data)
+
+########## Preprocessing Validation Dataset ##########
+dev_label = np.load("./data/dev_transcripts.npy", encoding='bytes')[:3]
+print("dev label", len(dev_label)) # 1106 utterances
+
+# Convert bytes to string
+for i in range(len(dev_label)):
+    dev_label[i] = dev_label[i].astype(str)
+print(dev_label)
+
+# Convert transcripts to character-based label
+dev_char_label = []
+for i in range(len(dev_label)):
+    tmp_label = []
+    tmp_label.append(LABEL_MAP['0'])
+    for j in range(len(dev_label[i])):
+        for k in range(len(list(dev_label[i][j]))):
+            key = list(dev_label[i][j])[k]
+            tmp_label.append(LABEL_MAP[key])
+        if j != len(dev_label[i]) - 1:
+            tmp_label.append(LABEL_MAP['<space>'])
+    tmp_label.append(LABEL_MAP['0'])
+    dev_char_label.append(tmp_label)
+print(dev_char_label)
+
+# Save characters-based label to file
+np.save('./data/dev_char.npy', np.array(dev_char_label))
