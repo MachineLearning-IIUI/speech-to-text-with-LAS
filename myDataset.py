27a1729534eb91c5d69d332bc2642173063ff96e
Yiwen Xu <yiwen@Yiwens-MacBook-Pro.local>
2018-11-25 20:58:01 -0500

    add <sos> <eos>

--- myDataset.py
+++ myDataset.py
@@ -2,26 +2,52 @@ import torch
 from torch.utils.data import Dataset
 import numpy as np
 
+from torch.utils.data import DataLoader
+
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
 class myDataset(Dataset):
     def __init__(self, data_path, transcripts_path):
         self.data = np.load(data_path, encoding='bytes')
+        self.flag = False
         if transcripts_path != None:
             self.label = np.load(transcripts_path)
-        else:
-            self.label = None
+            self.flag = True
     
     def __len__(self):
         return len(self.data) 
     
     def __getitem__(self, index):
         sequence = self.data[index]
-        sequence = torch.tensor(sequence)
-        if self.label != None:
+        sequence = torch.tensor(sequence).to(DEVICE)
+        if self.flag is True:
             target = self.label[index]
-            target = torch.tensor(target)
+            target = torch.tensor(target).to(DEVICE)
             return sequence, target
         else:
-            return sequence
+            return sequence, [-1]
+
+def collate_seq(seq_list):
+    inputs, targets = zip(*seq_list)
+    lens = [len(seq) for seq in inputs]
+    seq_order = sorted(range(len(lens)), key=lens.__getitem__, reverse=True)
+    inputs = [inputs[i] for i in seq_order]
+    targets = [targets[i] for i in seq_order]
+    return inputs, targets
+
+
+if __name__ == '__main__':
+    data_path = "./data/dev.npy"
+    transcripts_path = "./data/dev_char.npy"
+    train_set = myDataset(data_path, transcripts_path)
+    # for i in range(3):
+    #     sequence, targets = train_set.__getitem__(i)
+    #     print(sequence.shape) # seq_len * 40
+    train_loader = DataLoader(train_set, shuffle=False, batch_size=4, collate_fn=collate_seq, num_workers=4) 
+    for step, (inputs, targets) in enumerate(train_loader):
+        if step == 0:
+            print(inputs[0].shape, inputs[1].shape)
+            print(targets)
 
 
         
--- preprocessing.py
+++ preprocessing.py
@@ -3,6 +3,7 @@ import numpy as np
 ########## Preprocessing Training Dataset ##########
 train_data = np.load("./data/train.npy", encoding='bytes')
 train_label = np.load("./data/train_transcripts.npy", encoding='bytes') # 24724 utterances
+print("train label", len(train_label)) # 1106 utterances
 
 # Convert bytes to string
 for i in range(len(train_label)):
@@ -10,8 +11,8 @@ for i in range(len(train_label)):
 
 # Create vocabulary list and vocabulary map
 # character-based label
-LABEL_LIST = ['0'] # 33 characters
-LABEL_MAP = {'0':0}
+LABEL_LIST = ['<sos>', '<eos>'] # 34 characters
+LABEL_MAP = {'<eos>': 0, '<sos>': 1}
 for i in range(len(train_label)):
     for j in range(len(train_label[i])):
         for char in list(train_label[i][j]):
@@ -23,6 +24,7 @@ LABEL_MAP['<space>'] = len(LABEL_LIST) - 1
 LABEL_LIST = sorted(LABEL_LIST)
 for key in LABEL_MAP.keys():
     LABEL_MAP[key] = LABEL_LIST.index(key)
+
 print(LABEL_LIST)
 print(LABEL_MAP)
 
@@ -30,14 +32,14 @@ print(LABEL_MAP)
 char_label = []
 for i in range(len(train_label)):
     tmp_label = []
-    tmp_label.append(LABEL_MAP['0'])
+    tmp_label.append(LABEL_MAP['<sos>'])
     for j in range(len(train_label[i])):
         for k in range(len(list(train_label[i][j]))):
             key = list(train_label[i][j])[k]
             tmp_label.append(LABEL_MAP[key])
         if j != len(train_label[i]) - 1:
             tmp_label.append(LABEL_MAP['<space>'])
-    tmp_label.append(LABEL_MAP['0'])
+    tmp_label.append(LABEL_MAP['<eos>'])
     char_label.append(tmp_label)
 
 # Save characters-based label to file
@@ -50,28 +52,26 @@ data = np.load('./data/train_char.npy')
 assert len(data) == len(train_data)
 
 ########## Preprocessing Validation Dataset ##########
-dev_label = np.load("./data/dev_transcripts.npy", encoding='bytes')[:3]
+dev_label = np.load("./data/dev_transcripts.npy", encoding='bytes')
 print("dev label", len(dev_label)) # 1106 utterances
 
 # Convert bytes to string
 for i in range(len(dev_label)):
     dev_label[i] = dev_label[i].astype(str)
-print(dev_label)
 
 # Convert transcripts to character-based label
 dev_char_label = []
 for i in range(len(dev_label)):
     tmp_label = []
-    tmp_label.append(LABEL_MAP['0'])
+    tmp_label.append(LABEL_MAP['<sos>'])
     for j in range(len(dev_label[i])):
         for k in range(len(list(dev_label[i][j]))):
             key = list(dev_label[i][j])[k]
             tmp_label.append(LABEL_MAP[key])
         if j != len(dev_label[i]) - 1:
             tmp_label.append(LABEL_MAP['<space>'])
-    tmp_label.append(LABEL_MAP['0'])
+    tmp_label.append(LABEL_MAP['<eos>'])
     dev_char_label.append(tmp_label)
-print(dev_char_label)
 
 # Save characters-based label to file
 np.save('./data/dev_char.npy', np.array(dev_char_label))
